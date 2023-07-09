18d507c726bd62814bb2a3cf0e496656747d0b40
chunyean <chunyeanfoong@gmail.com>
2023-07-09 17:53:50 +0800

    set random answer to box

--- The-Power-of-Cals/page-2.js
+++ The-Power-of-Cals/page-2.js
@@ -12,13 +12,39 @@ const num2 = getRandomNum(10);
 const ques2 = document.getElementById("ques2");
 ques2.innerText = num2;
 
-//generate answer
+//function to generate an array with 3 random answer plus correct answer
+function randomUniqueNum(range, outputCount) {
+  let arr = [];
+  for (let i = 1; i <= range; i++) {
+    arr.push(i);
+  }
+  const index = arr.indexOf(answer);
+  arr.splice(index, 1);
+
+  let result = [];
+
+  for (let i = 1; i <= outputCount; i++) {
+    const random = Math.floor(Math.random() * (range - 1 - i));
+    result.push(arr[random]);
+    arr[random] = arr[range - 1 - i];
+  }
+
+  const idx = Math.floor(Math.random() * (result.length + 1));
+  result.splice(idx, 0, answer);
+  return result;
+}
+
+// function to put answer into random box
+function answerBox(boxAns) {
+  for (let j = 0; j <= 3; j++) {
+    boxAns[j].innerText = ranAns[j];
+  }
+}
+
+// get correct answer
 const answer = num1 + num2;
 const correctAnswer = document.getElementsByClassName("ans");
 
-console.log(correctAnswer);
-for (i = 0; i < correctAnswer.length; i++) {
-  let halo = correctAnswer[i].innerText;
-}
-
-halo = answer;
+//set all the answer into boxes
+const ranAns = randomUniqueNum(19, 3);
+answerBox(correctAnswer);
